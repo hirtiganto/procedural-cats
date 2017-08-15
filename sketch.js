6ab2f5f975279d30fe0239a1bf83bfc031c2fae2
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-08-15 21:41:11 +0200

    Add grid generating code
    Also added some testing code which will be replaced later

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
-function setup() {
+let gridSize = 32
+let grid
 
+function setup() {
+  makeGrid()
+  createCanvas(512, 512)
+  drawGrid()
+}
+
+function makeGrid() {
+  grid = new Array(32)
+  for (var i = 0; i < grid.length; i++) {
+    grid[i] = new Array(32)
+  }
+}
+
+function drawGrid() {
+  tileSize = floor(width / gridSize)
+
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid.length; j++) {
+      fill(random(51, 200))
+      stroke(0)
+      x = map(i, 0, 32, 0, 512)
+      y = map(j, 0, 32, 0, 512)
+      rect(x, y, tileSize, tileSize)
+    }
+  }
 }
 
 function draw() {
-  
+
 }
