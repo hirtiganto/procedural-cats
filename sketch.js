11f2be399dfb82618eeab2c9bd79ad93712ca0a7
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-08-15 22:28:22 +0200

    Redesign makeGrid() and drawGrid()
    Now this function is reusable so it can be used later in code. Also tiles by default have color 51

--- sketch.js
+++ sketch.js
@@ -2,16 +2,18 @@ let gridSize = 32
 let grid
 
 function setup() {
-  makeGrid()
+  grid = makeGrid(32, 32)
   createCanvas(512, 512)
   drawGrid()
 }
 
-function makeGrid() {
-  grid = new Array(32)
-  for (var i = 0; i < grid.length; i++) {
-    grid[i] = new Array(32)
+function makeGrid(x, y) {
+  let g = new Array(x)
+  for (var i = 0; i < g.length; i++) {
+    g[i] = new Array(y)
   }
+
+  return g
 }
 
 function drawGrid() {
@@ -19,15 +21,13 @@ function drawGrid() {
 
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid.length; j++) {
-      fill(random(51, 200))
-      stroke(0)
+      if (grid[x][y] === undefined) {
+        fill(51)
+        stroke(0)
+      }
       x = map(i, 0, 32, 0, 512)
       y = map(j, 0, 32, 0, 512)
       rect(x, y, tileSize, tileSize)
     }
   }
-}
-
-function draw() {
-
 }
