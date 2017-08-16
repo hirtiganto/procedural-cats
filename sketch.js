4079167f7e71123383dd9cc1c1df2b50bd64c1aa
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-08-16 12:11:52 +0200

    Add Cat class and makePaw() function, fix drawGrid bug

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function drawGrid() {
 
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid.length; j++) {
-      if (grid[x][y] === undefined) {
+      if (grid[i][j] === undefined) {
         fill(51)
         stroke(0)
       }
@@ -29,5 +29,33 @@ function drawGrid() {
       y = map(j, 0, 32, 0, 512)
       rect(x, y, tileSize, tileSize)
     }
+  }
+}
+
+class Cat {
+  constructor() {
+    console.log("Everything is under control")
+  }
+
+  makePaw() {
+    // generate variables that describe the paw
+    let length = parseInt(random(5, 14))
+    let thickness = parseInt(random(1, 3))
+    let pawHeight = parseInt(random(1, 2))
+
+    // make grid that will contain paw infos
+    let paw = makeGrid(thickness + 1, length)
+
+    // fill the grid with paw
+    for (var x = 0; x < paw.length; x++) {
+      for (var y = 0; y < paw[x].length; y++) {
+        if (x === 0 && y < paw[x].length - pawHeight) {
+          continue
+        }
+        paw[x][y] = color(255)
+      }
+    }
+
+    return paw
   }
 }
